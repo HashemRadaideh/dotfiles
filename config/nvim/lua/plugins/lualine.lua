7005e6e8c2f70d25eea10884f5e010c4ce5876a6
HashemRadaideh <hashemalradaideh@gmail.com>
2023-04-15 07:29:42 +0300

    New lualine style

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -79,10 +79,10 @@ lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = "", right = "" },
-    -- component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    -- section_separators = { left = "", right = "" },
+    -- component_separators = { left = "", right = "" },
+    -- section_separators = { left = "", right = "" },
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = { "alpha", "dashboard", "NvimTree", "Outline" },
       winbar = { "alpha", "dashboard", "NvimTree", "Outline" },
