ded124a411e236cc25086fd0b496a94c3eb607bb
HashemRadaideh <hashemalradaideh@gmail.com>
2023-05-08 23:46:36 +0300

    Moved fzdm to scripts and start graphical session in zprofile

--- config/zsh/configs/fzf.zsh
+++ config/zsh/configs/fzf.zsh
@@ -8,35 +8,6 @@ source /usr/share/fzf/key-bindings.zsh
 export FZF_DEFAULT_OPTS='--height=~50% --layout=reverse --border --cycle --preview="bat --decorations=always --color=always {} 2>/dev/null" --bind ctrl-u:preview-page-up,ctrl-d:preview-page-down --exit-0'
 export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git --exclude node_modules --exclude .cache'
 
-fzdm() {
-  if [[ -z "$TMUX" ]]; then
-    local list=("$(\ls "$XDG_DATA_HOME/sessions" | sort)" "tmux" "$(tty)" "power off" "reboot" "sleep" "logout")
-    local file=`printf "%s\n" "${list[@]}" | fzf --border-label=" Fuzzy Display Manager (fzdm) "`
-
-    starter() {
-      local extension="${$(grep -P 'export XDG_SESSION_TYPE=*' "$XDG_DATA_HOME/sessions/$1" | awk '{print $2}')//\)}"
-
-      if [[ "$extension" == "XDG_SESSION_TYPE=x11" ]]; then
-        startx "$XDG_DATA_HOME/sessions/$1"
-      else
-        eval "$(\cat $XDG_DATA_HOME/sessions/$1)"
-      fi
-    }
-
-    if [ -n "$file" ]; then
-      case "$file" in
-        "logout") exit ;;
-        "sleep") systemctl suspend ;;
-        "reboot") systemctl reboot ;;
-        "power off") systemctl poweroff ;;
-        "$(tty)") clear ;;
-        "tmux") exec fzt ;;
-        *) exec starter "$file" ;;
-      esac
-    fi
-  fi
-}
-
 ff() {
   temp=`fzf --prompt='Edit:'`
   if [[ "$temp" == "." ]]; then
--- config/zsh/profile.zsh
+++ config/zsh/profile.zsh
@@ -1,1 +1,36 @@
-zprofile() { }
+export DOTFILES="$HOME/.files"
+
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/bin"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+export XDG_DESKTOP_DIR="$HOME/Desktop"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_MUSIC_DIR="$HOME/Music"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_PUBLICSHARE_DIR="$HOME/Public"
+export XDG_TEMPLATES_DIR="$HOME/Templates"
+export XDG_VIDEOS_DIR="$HOME/Videos"
+
+# home directory clean up
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export GOPATH="$XDG_DATA_HOME/go"
+export GNUPGHOME="$XDG_DATA_HOME/gnupm"
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
+export GTK_2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_STYLE_OVERRIDE="kvantum"
+
+export PATH="$PATH:$XDG_DATA_HOME:$XDG_DATA_HOME/scripts:$XDG_DATA_HOME/sessions:$XDG_DATA_HOME/cargo/bin:$XDG_CONFIG_HOME/doom/bin:$XDG_CONFIG_HOME/emacs/bin:/usr/lib/jvm/java-19-openjdk/bin"
+
+zprofile() {
+  # Auto start tmux in ssh.
+  [ -x "$(command -v fzf)"  ] && [[ -n "$SSH_CONNECTION" ]] && exec fzt
+
+  if [[ -z "$DISPLAY" && $(tty) == /dev/tty1 ]] ; then
+    [ -x "$(command -v fzf)"  ] && exec fzdm
+  fi
+}
