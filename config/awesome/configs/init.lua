51aa9be145452874cfe0210830adc5a92f919479
HashemRadaideh <hashemalradaideh@gmail.com>
2023-05-09 16:47:52 +0300

    Replaced $TERM env variable with $TERMINAL

--- config/awesome/configs/init.lua
+++ config/awesome/configs/init.lua
@@ -6,7 +6,7 @@ awful.spawn.with_shell('autostart')
 File_manager = "pcmanfm"
 Graphical_editor = os.getenv("VISUAL")
 Shell = os.getenv("SHELL")
-Terminal_emulator = os.getenv("TERM")
+Terminal_emulator = os.getenv("TERMINAL")
 Terminal_editor = Terminal_emulator .. " -e " .. Shell .. " -c " .. os.getenv("EDITOR")
 Terminal_file_manager = Terminal_emulator .. " -e " .. Shell .. " -c " .. os.getenv("FILEMANAGER")
 Terminal_multiplexed = Terminal_emulator .. " -e " .. Shell .. " -c " .. "fzt"
