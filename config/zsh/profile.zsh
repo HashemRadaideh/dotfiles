9ea6eab0ef8baca97eb06da55a4fa0808b56cb6c
HashemRadaideh <hashemalradaideh@gmail.com>
2023-07-16 23:44:38 +0300

    Cleaned the zsh profile

--- config/zsh/profile.zsh
+++ config/zsh/profile.zsh
@@ -15,20 +15,37 @@ export XDG_TEMPLATES_DIR="$HOME/Templates"
 export XDG_VIDEOS_DIR="$HOME/Videos"
 
 # home directory clean up
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export ANDROID_HOME="$XDG_DATA_HOME"/android
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
-export GOPATH="$XDG_DATA_HOME/go"
 export GNUPGHOME="$XDG_DATA_HOME/gnupm"
-export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
+export GOPATH="$XDG_DATA_HOME/go"
+export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle                                                
 export GTK_2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export MINIKUBE_HOME="$XDG_DATA_HOME/minikube"
+export MPLAYER_HOME="$XDG_CONFIG_HOME/mplayer"
+export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history                                         
+export NUGET_PACKAGES="$XDG_CACHE_HOME"/NuGetPackages
+export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
 export QT_QPA_PLATFORMTHEME="qt5ct"
 export QT_STYLE_OVERRIDE="kvantum"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export SSB_HOME="$XDG_DATA_HOME"/zoom
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
+export WINEPREFIX="$HOME/wine"
+export _JAVA_OPTIONS=-Djava.io.tmpdir="$XDG_CONFIG_HOME"/java
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_RUNTIME_DIR/java"
 
 export PATH="$PATH:$XDG_DATA_HOME:$XDG_DATA_HOME/scripts:$XDG_DATA_HOME/sessions:$XDG_DATA_HOME/cargo/bin:$GOPATH/bin/:$XDG_CONFIG_HOME/doom/bin:$XDG_CONFIG_HOME/emacs/bin:/usr/lib/jvm/default/bin"
 
+alias mysql-workbench=mysql-workbench --configdir="$XDG_DATA_HOME/mysql/workbench"
+alias yarn='yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config'
+alias feh='feh --no-fehbg'
+alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
+
 zprofile() {
   # Auto start tmux in ssh.
-  [ -x "$(command -v fzf)"  ] && [[ -n "$SSH_CONNECTION" ]] && exec fzt
+  # [ -x "$(command -v fzf)"  ] && [[ -n "$SSH_CONNECTION" && -z $TMUX ]] && exec fzt
 
   if [[ -z "$DISPLAY" && $(tty) == /dev/tty1 ]] ; then
     [ -x "$(command -v fzf)"  ] && exec fzdm
