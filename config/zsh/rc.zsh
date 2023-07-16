aad9902d801da90f8221bd9bae36883ea92b66d7
HashemRadaideh <hashemalradaideh@gmail.com>
2023-07-16 23:44:14 +0300

    Moved from neofetch to macchina and added python venv auto start & opam

--- config/zsh/rc.zsh
+++ config/zsh/rc.zsh
@@ -10,7 +10,7 @@ zrc() {
   [ -x "$(command -v gh)"  ] && eval "$(gh completion -s zsh)"
 
   [ -x "$(command -v zoxide)"  ] && eval "$(zoxide init zsh)"
-  
+
   if [[ -z "$DISPLAY" ]] ; then
     # Fetch machine's specs.
     [ -x "$(command -v pfetch)"  ] && pfetch
@@ -20,6 +20,19 @@ zrc() {
     eval "$(starship init zsh)"
 
     # Fetch machine's specs.
-    [ -x "$(command -v neofetch)"  ] && neofetch
+    [ -x "$(command -v neofetch)"  ] && macchina
   fi
+
+  # opam configuration
+  [[ ! -r /home/hashem/.opam/opam-init/init.zsh ]] || source /home/hashem/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
+
+  python_venv() {
+    MYVENV=./.venv
+    # when you cd into a folder that contains $MYVENV
+    [[ -d $MYVENV ]] && source $MYVENV/bin/activate > /dev/null 2>&1
+    # when you cd into a folder that doesn't
+    [[ ! -d $MYVENV ]] && deactivate > /dev/null 2>&1
+  }
+  autoload -U add-zsh-hook
+  add-zsh-hook chpwd python_venv
 }
