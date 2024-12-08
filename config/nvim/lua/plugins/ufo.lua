3f914460d0e5374ae305f5232c706058b9c76d26
HashemRadaideh <hashemalradaideh@gmail.com>
2024-12-08 19:20:58 +0300

    Fixed folding in nvim

--- config/nvim/lua/plugins/ufo.lua
+++ config/nvim/lua/plugins/ufo.lua
@@ -5,23 +5,34 @@ return {
     "kevinhwang91/promise-async",
   },
   opts = {
-    provider_selector = function(bufnr, filetype, buftype)
-      return { "lsp", "treesitter", "indent" }
+    provider_selector = function()
+      return { "treesitter", "indent" }
     end,
   },
-  config = function()
-    vim.o.foldcolumn = "1"
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
-    vim.keymap.set("n", "zK", function()
-      local winid = require("ufo").peekFoldedLinesUnderCursor()
-      if not winid then
-        vim.lsp.buf.hover()
-      end
-    end, { desc = "Peek Fold" })
-  end,
+  keys = {
+    {
+      "zR",
+      function()
+        require("ufo").openAllFolds()
+      end,
+      { noremap = true, silent = true, desc = "Open all folds" },
+    },
+    {
+      "zM",
+      function()
+        require("ufo").closeAllFolds()
+      end,
+      { noremap = true, silent = true, desc = "Close all folds" },
+    },
+    {
+      "zK",
+      function()
+        local winid = require("ufo").peekFoldedLinesUnderCursor()
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end,
+      { noremap = true, silent = true, desc = "Peek Fold" },
+    },
+  },
 }
